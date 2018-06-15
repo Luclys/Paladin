0030619425cd480deae77a2f1c4db2ba099e581e
Luclys <spidloic@outlook.fr>
2018-06-15 18:03:28 +0200

    Add files via upload

--- index_Heroku.js
+++ index_Heroku.js
@@ -1,8 +1,7 @@
 Discord = require('discord.js');// Import the discord.js module
 const client = new Discord.Client();// Create an instance of a Discord client
-const config = require("./config.json");
 const fs = require("fs");
-const prefixe = config.prefixe;// Préfixe : défini sur "//"
+const prefixe = "//";
 // The ready event is vital, it means that your bot will only start reacting to information
 client.on('ready', () => {
   console.log('Paladin Bot Initialisé avec succès.');
